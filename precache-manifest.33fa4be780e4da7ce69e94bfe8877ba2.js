fb1dd8f854b257c8066e00843449f39c79e93fd9
Valentino Geronzi <valentinogeronzi@hotmail.com>
2022-11-18 11:59:37 -0300

    Updates

---
+++ precache-manifest.33fa4be780e4da7ce69e94bfe8877ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef1cb5167de1cd369d4",
+    "url": "/React-Ecommerce/static/css/main.b287b080.chunk.css"
+  },
+  {
+    "revision": "1ef1cb5167de1cd369d4",
+    "url": "/React-Ecommerce/static/js/main.6d51dffc.chunk.js"
+  },
+  {
+    "revision": "4130422d14803e13b2b9",
+    "url": "/React-Ecommerce/static/js/runtime~main.a7520d2f.js"
+  },
+  {
+    "revision": "cc62bc447865e050e65a",
+    "url": "/React-Ecommerce/static/css/2.936ce676.chunk.css"
+  },
+  {
+    "revision": "cc62bc447865e050e65a",
+    "url": "/React-Ecommerce/static/js/2.62eff48d.chunk.js"
+  },
+  {
+    "revision": "137a6b9ba11261badcea5c4516f89ff8",
+    "url": "/React-Ecommerce/index.html"
+  }
+];
