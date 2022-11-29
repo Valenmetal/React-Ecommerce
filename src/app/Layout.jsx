765e80be4afcebbdb9008f1b6085ca957b777e23
Valentino Geronzi <valentinogeronzi@hotmail.com>
2022-11-29 20:46:38 -0300

    Footer

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/NavBar/NavBar"
 import React from 'react'
+import Footer from "../components/Footer"
 
 const Layout = () => {
     return (
         <div>
             <NavBar />
             <Outlet />
+            <Footer />
         </div>
     )
 }
